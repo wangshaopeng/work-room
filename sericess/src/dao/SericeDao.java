7f3a4e87a7f832c492a3b90d72a175cd4bca40c1
wspsp <2506698200@qq.com>
2013-08-06 00:03:57 +0800

    delete

--- sericess/src/dao/SericeDao.java
+++ sericess/src/dao/SericeDao.java
@@ -6,12 +6,12 @@ import java.util.List;
 import entity.Serice;
 
 public interface SericeDao {
-	public List<Serice> getPageList(int currentPage,int lineSize); //分页
-	public int getCount(); //获取总数据总数量
-	public int getPage(int lineSize); //获取总页数
-	public int updateSerice(Serice s); //修改对象
-	public int addSerice(Serice s); //增加一条信息
-	public Serice getSericeWithId(int id);//按ID查询
-	public int DeleteSerice(int id);//删除
+	public List<Serice> getPageList(int currentPage,int lineSize); 
+	public int getCount(); 
+	public int getPage(int lineSize); 
+	public int updateSerice(Serice s); 
+	public int addSerice(Serice s);
+	public Serice getSericeWithId(int id);
+	public int DeleteSerice(int id);
 
 }
